03f9b8f51e96b32fb75ed84d6009760677bf4226
mohongxing <61826325+mohongxing@users.noreply.github.com>
2020-05-27 15:33:36 +0800

    第二周实验课实验2

---
+++ Expriment2-2/MFCExcepriment2-2.h
@@ -0,0 +1,33 @@
+﻿
+// MFCExcepriment2-2.h: MFCExcepriment2-2 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMFCExcepriment22App:
+// 有关此类的实现，请参阅 MFCExcepriment2-2.cpp
+//
+
+class CMFCExcepriment22App : public CWinApp
+{
+public:
+	CMFCExcepriment22App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCExcepriment22App theApp;
---
+++ Expriment2-2/MFCExcepriment2-2View.h
@@ -0,0 +1,49 @@
+﻿
+// MFCExcepriment2-2View.h: CMFCExcepriment22View 类的接口
+//
+
+#pragma once
+
+
+class CMFCExcepriment22View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCExcepriment22View() noexcept;
+	DECLARE_DYNCREATE(CMFCExcepriment22View)
+
+// 特性
+public:
+	CMFCExcepriment22Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCExcepriment22View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCExcepriment2-2View.cpp 中的调试版本
+inline CMFCExcepriment22Doc* CMFCExcepriment22View::GetDocument() const
+   { return reinterpret_cast<CMFCExcepriment22Doc*>(m_pDocument); }
+#endif
+
