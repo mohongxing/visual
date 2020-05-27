2f877bd5e84d82b846b52ea4e9ed0ac37c3e611d
mohongxing <61826325+mohongxing@users.noreply.github.com>
2020-05-27 15:24:22 +0800

    第二周课堂作业4

---
+++ Class2-4/MFCClass2-4View.cpp
@@ -0,0 +1,113 @@
+﻿
+// MFCClass2-4View.cpp: CMFCClass24View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCClass2-4.h"
+#endif
+
+#include "MFCClass2-4Doc.h"
+#include "MFCClass2-4View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCClass24View
+
+IMPLEMENT_DYNCREATE(CMFCClass24View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCClass24View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMFCClass24View 构造/析构
+
+CMFCClass24View::CMFCClass24View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCClass24View::~CMFCClass24View()
+{
+}
+
+BOOL CMFCClass24View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCClass24View 绘图
+
+void CMFCClass24View::OnDraw(CDC* pDC)
+{
+	CMFCClass24Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	int red = 100, green = 50, blue = 200;
+	int color = RGB(red, green, blue);
+	CBrush newBrush(color);
+	CBrush* oldBrush = pDC->SelectObject(&newBrush);
+	CRect cr;
+	GetClientRect(cr);
+	pDC->Ellipse(cr);
+	pDC->SelectObject(oldBrush);
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCClass24View 打印
+
+BOOL CMFCClass24View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCClass24View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFCClass24View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFCClass24View 诊断
+
+#ifdef _DEBUG
+void CMFCClass24View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCClass24View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCClass24Doc* CMFCClass24View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCClass24Doc)));
+	return (CMFCClass24Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCClass24View 消息处理程序
---
+++ Class2-4/MFCClass2-4View.h
@@ -0,0 +1,49 @@
+﻿
+// MFCClass2-4View.h: CMFCClass24View 类的接口
+//
+
+#pragma once
+
+
+class CMFCClass24View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCClass24View() noexcept;
+	DECLARE_DYNCREATE(CMFCClass24View)
+
+// 特性
+public:
+	CMFCClass24Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCClass24View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFCClass2-4View.cpp 中的调试版本
+inline CMFCClass24Doc* CMFCClass24View::GetDocument() const
+   { return reinterpret_cast<CMFCClass24Doc*>(m_pDocument); }
+#endif
+
