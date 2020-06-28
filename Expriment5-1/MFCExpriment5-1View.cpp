9cc6e48201e8197cfc132a86c6ab74b8835d7b29
mohongxing <61826325+mohongxing@users.noreply.github.com>
2020-06-28 23:12:29 +0800

    第五周实验课试验1

---
+++ Expriment5-1/MFCExpriment5-1View.cpp
@@ -0,0 +1,116 @@
+﻿
+// MFCExpriment5-1View.cpp: CMFCExpriment51View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCExpriment5-1.h"
+#endif
+
+#include "MFCExpriment5-1Doc.h"
+#include "MFCExpriment5-1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCExpriment51View
+
+IMPLEMENT_DYNCREATE(CMFCExpriment51View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCExpriment51View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_COMMAND(ID_SHOWNUMBER, &CMFCExpriment51View::OnShownumber)
+END_MESSAGE_MAP()
+
+// CMFCExpriment51View 构造/析构
+
+CMFCExpriment51View::CMFCExpriment51View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCExpriment51View::~CMFCExpriment51View()
+{
+}
+
+BOOL CMFCExpriment51View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCExpriment51View 绘图
+
+void CMFCExpriment51View::OnDraw(CDC* /*pDC*/)
+{
+	CMFCExpriment51Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCExpriment51View 打印
+
+BOOL CMFCExpriment51View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCExpriment51View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CMFCExpriment51View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CMFCExpriment51View 诊断
+
+#ifdef _DEBUG
+void CMFCExpriment51View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCExpriment51View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCExpriment51Doc* CMFCExpriment51View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCExpriment51Doc)));
+	return (CMFCExpriment51Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCExpriment51View 消息处理程序
+
+
+void CMFCExpriment51View::OnShownumber()
+{
+	// TODO: 在此添加命令处理程序代码
+	CString s;
+	CClientDC dc(this);
+	s = _T("201812300073,莫鸿杏");
+	dc.TextOutW(100, 200, s);
+}
---
+++ Expriment5-1/MFCExpriment5-1View.h
@@ -0,0 +1,51 @@
+﻿
+// MFCExpriment5-1View.h: CMFCExpriment51View 类的接口
+//
+
+#pragma once
+
+
+class CMFCExpriment51View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCExpriment51View() noexcept;
+	DECLARE_DYNCREATE(CMFCExpriment51View)
+
+// 特性
+public:
+	CMFCExpriment51Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMFCExpriment51View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnShownumber();
+};
+
+#ifndef _DEBUG  // MFCExpriment5-1View.cpp 中的调试版本
+inline CMFCExpriment51Doc* CMFCExpriment51View::GetDocument() const
+   { return reinterpret_cast<CMFCExpriment51Doc*>(m_pDocument); }
+#endif
+
